e3f8b1294d9a306108b1f10754667b30dcaea547
guilherme <guilherme@localhost>
2016-09-15 18:03:02 -0300

    integraçao do thema com o codeigniter

--- application/views/painel/pages/index.php
+++ application/views/painel/pages/index.php
@@ -1,44 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="pt-br">
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>SB Admin 2 - Bootstrap Admin Theme</title>
-
+    <title>Kairos</title>
     <!-- Bootstrap Core CSS -->
-    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
+    <link href="<?= base_url('assets/painel/vendor/bootstrap/css/bootstrap.min.css')?>" rel="stylesheet">
     <!-- MetisMenu CSS -->
-    <link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
-
+    <link href="<?= base_url('assets/painel/vendor/metisMenu/metisMenu.min.css')?>" rel="stylesheet">
     <!-- Custom CSS -->
-    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
-
+    <link href="<?= base_url('assets/painel/dist/css/sb-admin-2.css')?>" rel="stylesheet">
     <!-- Morris Charts CSS -->
-    <link href="../vendor/morrisjs/morris.css" rel="stylesheet">
-
+    <link href="<?= base_url('assets/painel/vendor/morrisjs/morris.css')?>" rel="stylesheet">
     <!-- Custom Fonts -->
-    <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
+    <link href="<?= base_url('assets/painel/vendor/font-awesome/css/font-awesome.min.css')?>" rel="stylesheet" type="text/css">
 </head>
-
 <body>
-
     <div id="wrapper">
-
         <!-- Navigation -->
         <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
             <div class="navbar-header">
@@ -48,7 +27,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.html">SB Admin v2.0</a>
+                <a class="navbar-brand" href="index.php">SB Admin v2.0</a>
             </div>
             <!-- /.navbar-header -->
 
@@ -916,21 +895,21 @@
     <!-- /#wrapper -->
 
     <!-- jQuery -->
-    <script src="../vendor/jquery/jquery.min.js"></script>
+    <script src="<?= base_url('assets/painel/vendor/jquery/jquery.min.js')?>"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="<?= base_url('assets/painel/vendor/bootstrap/js/bootstrap.min.js')?>"></script>
 
     <!-- Metis Menu Plugin JavaScript -->
-    <script src="../vendor/metisMenu/metisMenu.min.js"></script>
+    <script src="<?= base_url('assets/painel/vendor/metisMenu/metisMenu.min.js')?>"></script>
 
     <!-- Morris Charts JavaScript -->
-    <script src="../vendor/raphael/raphael.min.js"></script>
-    <script src="../vendor/morrisjs/morris.min.js"></script>
-    <script src="../data/morris-data.js"></script>
+    <script src="<?= base_url('assets/painel/vendor/raphael/raphael.min.js')?>"></script>
+    <script src="<?= base_url('assets/painel/vendor/morrisjs/morris.min.js')?>"></script>
+    <script src="<?= base_url('assets/painel/data/morris-data.js')?>"></script>
 
     <!-- Custom Theme JavaScript -->
-    <script src="../dist/js/sb-admin-2.js"></script>
+    <script src="<?= base_url('assets/painel/dist/js/sb-admin-2.js')?>"></script>
 
 </body>
 
